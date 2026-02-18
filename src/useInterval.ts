0107dd5f0609e2361fc08b63933df0b77d0c8025
Sebastian Vittersø <svit@equinor.com>
2026-02-18 10:33:31 +0100

    refactor: rename delay->delayMs

--- src/useInterval.ts
+++ src/useInterval.ts
@@ -5,14 +5,14 @@ import { useEffect, useMemo, useRef } from 'react'
  * If the system goes to sleep and wakes up later, the callback will be executed
  * immediately (within 100ms) after waking up if the specified delay has passed.
  */
-export function useInterval(callback: () => void, delay: number, maxRandomStaggerMs: number): void {
+export function useInterval(callback: () => void, delayMs: number, maxRandomStaggerMs: number): void {
   const lastTime = useRef(Date.now())
 
   const randomStagger = useMemo(() => Math.random() * maxRandomStaggerMs, [maxRandomStaggerMs])
 
   function tick() {
     const now = Date.now()
-    const totalDelay = delay + Math.random() * randomStagger
+    const totalDelay = delayMs + Math.random() * randomStagger
     if (now - lastTime.current >= totalDelay) {
       callback()
       lastTime.current = now
@@ -23,5 +23,5 @@ export function useInterval(callback: () => void, delay: number, maxRandomStagge
     tick() // Tick immediately on mount/effect, instead of waiting for the first interval to pass
     const interval = setInterval(tick, 100) // Run tick every 100ms to check if the specified delay has passed
     return () => clearInterval(interval)
-  }, [callback, delay, randomStagger])
+  }, [callback, delayMs, randomStagger])
 }
